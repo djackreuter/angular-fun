37006c8786c96099990e019cd8a9755ba8cdd5f9
Dylan Reuter <djreuter45@gmail.com>
2017-10-04 14:07:11 -0600

    updated user component

--- angular-fun/src/app/components/user/user.component.ts
+++ angular-fun/src/app/components/user/user.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+	selector: 'app-user',
+	templateUrl: './user.component.html',
+	styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+	name: string;
+	age: number;
+	email: string;
+	address:Address;
+	hobbies:string[];
 
-  constructor() { }
+	constructor() {
+	}
 
-  ngOnInit() {
-  }
+	ngOnInit() {
 
+		this.name = 'John Doe';
+		this.age = 25;
+		this.address = {
+			street: '200 Main St',
+			city: 'Albuquerque',
+			state: 'New Mexico'
+		}
+		this.hobbies = ['Listen to music', 'Write code', 'Watch movies'];
+	}
+
+}
+
+
+interface Address {
+	street: string,
+	city:string,
+	state:string
 }
