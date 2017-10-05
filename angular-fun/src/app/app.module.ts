f1371702f8e151a56bbd780c0a64608a3edda78f
Dylan Reuter <djreuter45@gmail.com>
2017-10-04 20:28:11 -0600

    updated about component

--- angular-fun/src/app/app.module.ts
+++ angular-fun/src/app/app.module.ts
@@ -1,12 +1,18 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
+import {RouterModule, Routes} from "@angular/router";
 import {AppComponent} from './app.component';
 import {UserComponent} from './components/user/user.component';
 import {FormsModule} from "@angular/forms";
 import {DataService} from "./services/data.service";
 import {HttpModule} from "@angular/http";
-import { AboutComponent } from './components/about/about.component';
+import {AboutComponent} from './components/about/about.component';
+
+const appRoutes: Routes = [
+	{path:'', component:UserComponent},
+	{path:'about', component:AboutComponent}
+];
+
 
 @NgModule({
 	declarations: [
@@ -17,7 +23,8 @@ import { AboutComponent } from './components/about/about.component';
 	imports: [
 		BrowserModule,
 		FormsModule,
-		HttpModule
+		HttpModule,
+		RouterModule.forRoot(appRoutes)
 	],
 	// add newly created data service to providers and import
 	providers: [DataService],
